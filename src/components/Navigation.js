3017b21c8b53f9ed8ded181aa8d26848a1e36459
andrewsbusby <andrewsbusby@gmail.com>
2021-06-17 22:50:03 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/components/Navigation.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { CartContext } from '../context/CartContext';
+import { useContext } from 'react';
+
+const Navigation = () => {
+
+	const cart = useContext(CartContext);
+
+	return (
+		<div className="navigation">
+			<NavLink to="/">Products</NavLink>
+			<NavLink to="/cart">
+				Cart <span>{cart.length}</span>
+			</NavLink>
+		</div>
+	);
+};
+
+export default Navigation;
---
+++ src/components/Products.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { useContext } from 'react';
+import { ProductContext } from '../context/ProductContext';
+// Components
+import Product from './Product';
+
+const Products = () => {
+
+		const {products, addItem} = useContext(ProductContext);
+
+	return (
+		<div className="products-container">
+			{products.map(product => (
+				<Product
+					key={product.id}
+					product={product}
+					addItem={addItem}
+				/>
+			))}
+		</div>
+	);
+};
+
+export default Products;
